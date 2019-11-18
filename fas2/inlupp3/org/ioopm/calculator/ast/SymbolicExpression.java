79e375f1c25342beae8ef79f0e0962b644c100f3
Jonathan <jonathannoahtadese@gmail.com>
2019-11-18 15:24:54 +0100

    added documentation to SymbolicExpression

--- fas2/inlupp3/org/ioopm/calculator/ast/SymbolicExpression.java
+++ fas2/inlupp3/org/ioopm/calculator/ast/SymbolicExpression.java
@@ -1,31 +1,84 @@
 package org.ioopm.calculator.ast;
 import java.util.HashMap;
 
+/**
+ * @file SymbolicExpression.java 
+ * @author Patrik Johansson, Jonathan Tadese 
+ * @date 15-11-2019
+ * @class SymbolicExpression
+ * @brief Root class of hierarchy. Defines common expressions for all operations of the calculator.
+*/
+
 public abstract class SymbolicExpression {
+
+// **************************************************
+// Constructor
+// **************************************************
+
+
+    /**
+     * @brief Default constructor 
+     */
     public SymbolicExpression () {	
     }
 
+// **************************************************
+// Public methods
+// **************************************************
+
+    /**
+     * @brief Determines if object is constant
+     * @return true if object is constant, else false
+     */
     public boolean isConstant(){
 	return false;
     }
+
+    /**
+     * @brief Determines if object is variable
+     * @return true if object is variable, else false
+     */
     public boolean isVariable(){
 	return false;
     }
+
+    /**
+     * @brief Determines if object is command
+     * @return true if object is command, else false
+     */
     public boolean isCommand(){
 	return false;
     }
+
     
+    /**
+     * @brief Retrieve operator name, i.e. "sin", "cos", or "+"
+     * @return Operator name if subclass contains overridden method, else throws a RuntimeException
+     */
     public String getName(){
 	throw new RuntimeException("getName() called on expression with no operator");
     }
 
+    /**
+     * @brief Determines precedence order of where to use parenthesis
+     * @return The priority of the class
+     */
     public int getPriority() {
 	return 0;
     }
 
+    /**
+     * @brief Get a constant value
+     * @return Constant value if subclass contains overriden method, else throws RuntimeException
+     */
     public Double getValue(){
 	throw new RuntimeException("getValue() called on non constant expression");
     }
 
+    /**
+     * @brief Evaluates SymbolicExpressions. Is overridden in subclasses
+     * @param vars Hashmap containing every SymbolicExpression
+     * @return A new SymbolicExpression
+     */
     public abstract SymbolicExpression eval(Environment vars);
 }
